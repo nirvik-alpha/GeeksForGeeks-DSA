1cf4b54904d4f22c4e49a8593be0882b59c2a40d
Sadid Rafan <62620773+nirvik-alpha@users.noreply.github.com>
2023-04-25 05:31:48 +0600

    Added solution - LeetHub

--- Missing number - GFG/missing-number.cpp
+++ Missing number - GFG/missing-number.cpp
@@ -25,19 +25,19 @@ int main()
 
 int missingNumber(int A[], int N)
 {
-  int xor1=0;
+  int hash[N+1] = {0};
   
-  for(int i=1;i<=N;i++)
-  {
-      xor1=xor1^i;
-  }
-  int xor2 = 0;
   for(int i=0;i<N-1;i++)
   {
-      xor2 = xor2^A[i];
+      hash[A[i]]++;
   }
-  
-  return (xor1^xor2);
-   
+  for(int i=1;i<=N;i++)
+  {
+      if(hash[i]==0){
+      return i;    
+      }
+      
+  }
+  return -1;
    
 }
